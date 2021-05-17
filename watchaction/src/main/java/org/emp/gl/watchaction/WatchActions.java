aef08fd4e18e16e7a4bb462c9aee266f556fa3ee
ioua56 <60513477+ioua56@users.noreply.github.com>
2021-05-17 21:34:32 +0100

    Add files via upload

---
+++ watchaction/src/main/java/org/emp/gl/watchaction/WatchActions.java
@@ -0,0 +1,14 @@
+package org.emp.gl.watchaction;
+
+/**
+ * Hello world!
+ *
+ */
+public interface WatchActions 
+{
+    public void doConfigue();
+    
+    public void doMode();
+    
+    public void doIncrement();
+}
